ac8acfa138fe2a2b6d4e5386548643461bf54b79
Mohd Azgar Ali <azgar53ali@gmail.com>
2023-12-12 11:59:14 +0530

    updaded note css and added createArea title display functionality

--- src/Component/CreateArea/CreateArea.jsx
+++ src/Component/CreateArea/CreateArea.jsx
@@ -8,12 +8,13 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 
 const CreateArea=(props)=>{
     const tabScreen = useMediaQuery('(max-width:759px)')
+    const [noteAreaFocus, setNoteAreaFocus] = useState(false)
     const [note, setNote]= useState({
         title:'',
         content:''
     })
 
-    function handleChange(event){
+    function handleChange(event){   
         const {name, value} = event.target;
         setNote(prevNote=>{
             return{
@@ -35,8 +36,8 @@ const CreateArea=(props)=>{
     return(
         <div className={styles.createAreaMainDiv}>
             <div className={styles.createArea}>
-                <input name="title" onChange={handleChange} value={note.title} placeholder='Title' className={styles.title}/>
-                <textarea name="content" value={note.content} placeholder="Take a Note" className={styles.textArea} onChange={handleChange}/>
+                <input name="title" onChange={handleChange} value={note.title} placeholder='Title' className={styles.title} style={{display: noteAreaFocus?'block':'none'}}/>
+                <textarea name="content" value={note.content} placeholder="Take a Note" className={styles.textArea} onChange={handleChange} onFocus={()=>setNoteAreaFocus(true)} onBlur={()=>setNoteAreaFocus(false)}/>
                 <Fab size={tabScreen?"small":"medium"} aria-label="add" className={styles.addBtn} onClick={submitNote}>
                     <AddIcon />
                 </Fab>
